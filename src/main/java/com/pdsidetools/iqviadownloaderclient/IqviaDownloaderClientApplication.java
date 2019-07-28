0fc6b0401e97b134f3ef4ee15c592710ea418c76
Minda Xiang <mxiang@transperfect.com>
2019-07-28 16:23:44 -0400

    add multi-profile, add arguments parsing

--- src/main/java/com/pdsidetools/iqviadownloaderclient/IqviaDownloaderClientApplication.java
+++ src/main/java/com/pdsidetools/iqviadownloaderclient/IqviaDownloaderClientApplication.java
@@ -23,22 +23,46 @@ public class IqviaDownloaderClientApplication implements CommandLineRunner {
 
 	@Override
     public void run(String... args) {
-		
+
         if(args.length < 2) {
         	System.out.println("argument number should be at least 2.");
         	return;
         }
         
-        String id_str = args[0];
+        String id_str = null;
+        File targetFolder = null;
+        
+        for(String arg : args){
+        	String[] parts = arg.split("=");
+        	if(parts.length != 2){
+        		continue;
+        	}
+        	String name = parts[0].trim();
+        	String value = parts[1].trim();
+        	
+        	if(name.equals("ids")){
+        		id_str = value;
+        	}else if(name.equals("target_folder")){
+        		targetFolder = new File(value);
+        	}
+        }
+        
+        if(id_str == null){
+        	System.out.println("submission id values not specified");
+        	return;
+        }
         String[] ids = Arrays.stream(id_str.split(",")).map(String::trim).toArray(String[]::new);
         if(ids == null || ids.length == 0){
         	System.out.println("please specify at least 1 submission id");
         	return;
         }
         
-        File targetFolder = new File(args[1].trim());
+        if(targetFolder == null){
+        	System.out.println("target folder values not specified");
+        	return;
+        }
         if(!targetFolder.exists() || !targetFolder.isDirectory()){
-        	System.out.println("target folder path not exist or invalid.");
+        	System.out.println("target folder path not exist or invalid: " + targetFolder.getAbsolutePath());
         	return;
         }
         
